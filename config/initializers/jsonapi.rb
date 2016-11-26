0394c9dcce2f6c7a4bbb236b403eae7fdc5af8be
Jacob Courtneay <jacob@sporkexec.com>
2016-11-26 00:43:52 -0600

    Enable pagination

--- config/initializers/jsonapi.rb
+++ config/initializers/jsonapi.rb
@@ -1,3 +1,6 @@
 JSONAPI.configure do |config|
+  config.default_paginator = :paged
+  config.top_level_meta_include_page_count = true
+  config.top_level_meta_include_record_count = true
   config.resource_key_type = :uuid
 end
